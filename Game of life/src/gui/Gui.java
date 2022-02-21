3e306a475afff84db70d7d341f1c8ab85d807896
MartinMagino <100129741+MartinMagino@users.noreply.github.com>
2022-02-21 11:57:46 +0100

    Add files via upload

---
+++ Game of life/src/gui/Gui.java
@@ -0,0 +1,26 @@
+package gui;
+
+import draw.Draw;
+
+import javax.swing.*;
+
+public class Gui {
+    JFrame jf;
+    public static Draw d;
+
+    public static final int WIDTH = 512,HIGHT = 512,XOFF = 10,YOFF = 10;
+    public void create(){
+        jf = new JFrame("Game of Life");
+        jf.setSize(550,570);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLocationRelativeTo(null);
+        jf.setResizable(false);
+
+        d= new Draw();
+        d.setBounds(0,0,550,570);
+        d.setVisible(true);
+        jf.add(d);
+
+        jf.setVisible(true);
+    }
+}
